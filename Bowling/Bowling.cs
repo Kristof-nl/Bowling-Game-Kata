be791ed1612773232cf340f757442a32ab674a8a
K. Hermaszewski <hermaszewski.krzysztof@gmail.com>
2021-11-02 17:21:05 +0100

    Change strike and spare

--- Bowling/Bowling.cs
+++ Bowling/Bowling.cs
@@ -9,19 +9,39 @@ namespace Bowling
     class Bowling
     {
         int totalScore = 0;
+        int nextRoll = 0;
         int lastRoll = 0;
-        int secondLastRoll = 0;
+        int secondNextRoll = 0;
         int frame = 0;
         int nubRolls = 0;
+        bool strike = false;
+        bool spare = false;
+        int numStrikes = 0;
 
         public void roll(int pins)
         {
             totalScore += pins;
             nubRolls++;
             {
+                if (spare && frame != 10)
+                {
+                    totalScore += pins;
+                    spare = false;
+                }
+
+                if (strike && frame != 10)
+                {
+                    totalScore += pins;
+                    numStrikes++;
+                    if (numStrikes == 2)
+                    {
+                        strike = false;
+                    }
+                }
+
                 if (pins == 10 && nubRolls == 1)
                 {
-                    totalScore += lastRoll + secondLastRoll;
+                    strike = true;
                     frame++;
                     nubRolls = 0;
                 }
@@ -32,18 +52,36 @@ namespace Bowling
                 }
                 else if (nubRolls == 2 && pins + lastRoll == 10)
                 {
-                    totalScore += lastRoll;
+                    spare = true;
                     nubRolls = 0;
                     frame++;
                 }
             }
-            secondLastRoll = lastRoll;
             lastRoll = pins;
+            tenFrame();
 
         }
         public int score()
         {
             return totalScore;
         }
+
+        public void tenFrame()
+        {
+            if (frame == 10 && spare)
+            {
+                Console.Write("You have spare in the last frame. Roll one more time: ");
+                int extraSpareScore = Convert.ToInt32(Console.ReadLine());
+                totalScore += extraSpareScore;
+            }
+            else if ((frame == 10 && strike))
+            {
+                Console.Write("You have string in the last frame. You have two extra rolls: ");
+                int extraStrikeScore1 = Convert.ToInt32(Console.ReadLine());
+                int extraStrikeScore2 = Convert.ToInt32(Console.ReadLine());
+                totalScore += extraStrikeScore1 + extraStrikeScore2;
+            }
+        }
+        
     }
 }
